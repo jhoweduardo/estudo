54317bf84fe43fe0a09f9c3e6bb2f20ebfdb82c8
Jhonatan Eduardo <jhonatan.eduardo34@gmail.com>
2025-05-09 17:48:04 -0300

    Singleton

---
+++ DesignPatterns/Singleton/Singleton/Program.cs
@@ -0,0 +1,13 @@
+﻿using Singleton;
+
+Console.WriteLine("Qual seu nome?");
+var nome = Console.ReadLine();
+
+Console.WriteLine("Qual seu email?");
+var email = Console.ReadLine();
+
+var session = SessionManager.Instance;
+
+session.IniciarSessao(nome, email);
+
+session.ExibirSessao();
---
+++ DesignPatterns/Singleton/Singleton/SessionManager.cs
@@ -0,0 +1,37 @@
+﻿namespace Singleton
+{
+    public class SessionManager
+    {
+        private static SessionManager instance;
+        private static readonly object _lock = new object();
+        private SessionManager() { }
+
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public DateTime HorarioLogin { get; set; }
+
+        public static SessionManager Instance
+        {
+            get {
+                lock (_lock) 
+                if (instance == null)
+                    instance = new SessionManager();
+
+                return instance; }
+        }
+
+        public void IniciarSessao(string nome, string email)
+        {
+            Nome = nome;
+            Email = email;
+            HorarioLogin = DateTime.Now;
+        }
+
+        public void ExibirSessao()
+        {
+            Console.WriteLine($"Nome: {Nome}");
+            Console.WriteLine($"Email: {Email}");
+            Console.WriteLine($"Horario de login: {HorarioLogin}");
+        }
+    }
+}
